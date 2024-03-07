fa0092f7cfd3c5ce4cc81cab033b557e15108927
Giulia Corrado <giulia.corrado02@gmail.com>
2024-03-07 17:03:28 +0100

    Add files via upload

---
+++ Exercises/Esercizio Pasticceria/AutomaticBakery.java
@@ -0,0 +1,138 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AutomaticBakery {
+  public static void main(String[] args) {
+    String[] base = { "Crumble", "Bisquit", "Sponge Cake" };
+    String[] stuffing = { "Pistachio Cream", "Nutella", "Egg Cream" };
+    String[] fruit = { "Banana", "Strawberry", "No fruit" };
+    String[] topping = { "Powder Sugar", "Sprinkles", "No topping" };
+
+    Scanner myScan = new Scanner(System.in);
+    ArrayList<String> cake = new ArrayList<String>();
+
+    /*
+     * 
+     * int userStuffing = myScan.nextInt();
+     * int userFruit = myScan.nextInt();
+     * int userTopping = myScan.nextInt();
+     */
+    System.out.println("");
+    System.out.println("Welcome to the best bakery in town!");
+    System.out.println("To make a cake, you must choose one of each of the following choices to compose your cake!");
+    System.out.println("Are you ready? Let's get Started!");
+    System.out.println("");
+
+    // User Base choice
+    System.out.println("Choose a base for your cake:");
+    for (int b = 0; b < base.length; b++) {
+      System.out.print("[" + (b + 1) + "]" + base[b] + " ");
+    }
+    System.out.println("");
+    int userBase = myScan.nextInt();
+    // This condition occurs when user chooses a number < 1 or > 3.
+    if (userBase < 1 || userBase > 3) {
+      System.out.println("The option you entered doesn't exsist! Choose again!");
+      userBase = myScan.nextInt();
+    }
+    switch (userBase) {
+      case 1:
+        System.out.println("You chose the following base: " + base[userBase - 1]);
+        break;
+      case 2:
+        System.out.println("You chose the following base: " + base[userBase - 1]);
+        break;
+      case 3:
+        System.out.println("You chose the following base: " + base[userBase - 1]);
+        break;
+    }
+    cake.add(base[userBase - 1]);
+    // System.out.println(cake.get(0)); // to check if the item has been pushed into
+    // ArrayList cake
+
+    // User Stuffing choice
+    System.out.println("");
+    System.out.println("Choose a stuffing for your cake:");
+    for (int b = 0; b < stuffing.length; b++) {
+      System.out.print("[" + (b + 1) + "]" + stuffing[b] + " ");
+    }
+    int userStuffing = myScan.nextInt();
+    // This condition occurs when user chooses a number < 1 or > 3.
+    if (userStuffing < 1 || userStuffing > 3) {
+      System.out.println("The option you entered doesn't exsist! Choose again!");
+      userStuffing = myScan.nextInt();
+    }
+    switch (userStuffing) {
+      case 1:
+        System.out.println("You chose the following stuffing: " + stuffing[userStuffing - 1]);
+        break;
+      case 2:
+        System.out.println("You chose the following stuffing: " + stuffing[userStuffing - 1]);
+        break;
+      case 3:
+        System.out.println("You chose the following stuffing: " + stuffing[userStuffing - 1]);
+        break;
+    }
+    cake.add(stuffing[userStuffing - 1]);
+    // System.out.println(cake.get(1)); // to check if the item has been pushed into
+    // ArrayList cake
+
+    // User Fruit choice
+    System.out.println("");
+    System.out.println("Choose a fruit for your cake:");
+    for (int b = 0; b < fruit.length; b++) {
+      System.out.print("[" + (b + 1) + "]" + fruit[b] + " ");
+    }
+    int userFruit = myScan.nextInt();
+    // This condition occurs when user chooses a number < 1 or > 3.
+    if (userFruit < 1 || userFruit > 3) {
+      System.out.println("The option you entered doesn't exsist! Choose again!");
+      userFruit = myScan.nextInt();
+    }
+    switch (userFruit) {
+      case 1:
+        System.out.println("You chose the following fruit: " + fruit[userFruit - 1]);
+        break;
+      case 2:
+        System.out.println("You chose the following fruit: " + fruit[userFruit - 1]);
+        break;
+      case 3:
+        System.out.println("You chose to not put fruit");
+        break;
+    }
+    cake.add(fruit[userFruit - 1]);
+    // System.out.println(cake.get(2)); // to check if the item has been pushed into
+    // ArrayList cake
+
+    // User Topping choice
+    System.out.println("");
+    System.out.println("Choose a topping for your cake:");
+    for (int b = 0; b < topping.length; b++) {
+      System.out.print("[" + (b + 1) + "]" + topping[b] + " ");
+    }
+    int userTopping = myScan.nextInt();
+    // This condition occurs when user chooses a number < 1 or > 3.
+    if (userTopping < 1 || userTopping > 3) {
+      System.out.println("The option you entered doesn't exsist! Choose again!");
+      userTopping = myScan.nextInt();
+    }
+    switch (userTopping) {
+      case 1:
+        System.out.println("You chose the following topping: " + topping[userTopping - 1]);
+        break;
+      case 2:
+        System.out.println("You chose the following topping: " + topping[userTopping - 1]);
+        break;
+      case 3:
+        System.out.println("You chose to not put topping");
+        break;
+    }
+    cake.add(topping[userTopping - 1]);
+    // System.out.println(cake.get(2)); // to check if the item has been pushed into
+    // ArrayList cake
+    System.out.println("");
+    System.out.println("Your cake is now ready!");
+    System.out.println("Your cake has " + cake.get(0) + " as base, " + cake.get(1) + " as filling, " + cake.get(2)
+        + " and " + cake.get(3));
+  }
+}
